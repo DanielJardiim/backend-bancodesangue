a7bcd85ae5c9580a0201d771b8734403c0d9af23
DanielJardiim <jn_dani@outlook.com>
2022-10-28 10:53:18 -0300

    modified:   src/port/users_api.js, lógica do sistema de armazenamento e capacidade do banco de sangue

--- src/port/users_api.js
+++ src/port/users_api.js
@@ -1,5 +1,14 @@
 const User = require('../models/User');
 
+var numAmais = 0;
+var numAmenos = 0;
+var numBmais = 0;
+var numBmenos = 0;
+var numABmais = 0;
+var numABmenos = 0;
+var numOmais = 0;
+var numOmenos = 0;
+
 module.exports = app => {
     // CREATE
     app.post('/bancodesangue/create', async (req, res) => {
@@ -40,7 +49,55 @@ module.exports = app => {
         if (!user) {
             try {
                 await User.create(userSchema);
-                return res.status(201).json({ msg: 'User create - OK', email });
+
+                if (tipoSanguineo === 'A+') {
+                    const Amais = ((++numAmais / 1000) * 100).toFixed(2);
+                    return res
+                        .status(201)
+                        .json({ msg: 'User create - OK', Amais });
+                }
+                if (tipoSanguineo === 'A-') {
+                    const Amenos = ((++numAmenos / 1000) * 100).toFixed(2);
+                    return res
+                        .status(201)
+                        .json({ msg: 'User create - OK', Amenos });
+                }
+                if (tipoSanguineo === 'B+') {
+                    const Bmais = ((++numBmais / 1000) * 100).toFixed(2);
+                    return res
+                        .status(201)
+                        .json({ msg: 'User create - OK', Bmais });
+                }
+                if (tipoSanguineo === 'B-') {
+                    const Bmenos = ((++numBmenos / 1000) * 100).toFixed(2);
+                    return res
+                        .status(201)
+                        .json({ msg: 'User create - OK', Bmenos });
+                }
+                if (tipoSanguineo === 'AB+') {
+                    const ABmais = ((++numABmais / 1000) * 100).toFixed(2);
+                    return res
+                        .status(201)
+                        .json({ msg: 'User create - OK', ABmais });
+                }
+                if (tipoSanguineo === 'AB-') {
+                    const ABmenos = ((++numABmenos / 1000) * 100).toFixed(2);
+                    return res
+                        .status(201)
+                        .json({ msg: 'User create - OK', ABmenos });
+                }
+                if (tipoSanguineo === 'O+') {
+                    const Omais = ((++numOmais / 1000) * 100).toFixed(2);
+                    return res
+                        .status(201)
+                        .json({ msg: 'User create - OK', Omais });
+                }
+                if (tipoSanguineo === 'O-') {
+                    const Omenos = ((++numOmenos / 1000) * 100).toFixed(2);
+                    return res
+                        .status(201)
+                        .json({ msg: 'User create - OK', Omenos });
+                }
             } catch (error) {
                 return res.status(500).json({ error: error });
             }
