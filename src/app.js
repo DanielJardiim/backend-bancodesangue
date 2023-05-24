c638e214563bb7f8f22810ded98e1605fcda4598
guilherme <guilhermedmlopes@hotmail.com>
2023-05-23 22:32:28 -0300

    Implementado middlewares, tratamento de erro e validação de campos vazios em nossa aplicação

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js";
+import manipuladorDeErros from "./middlewares/manipuladorDeErros.js";
+import manipulador404 from "./middlewares/manipulador404.js";
+
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => {
@@ -13,10 +16,10 @@ app.use(express.json());
 
 routes(app);
 
-//Middleware para interceptar qualquer erro identificado pela nossa aplicação em toda requisição da nossa API
-// eslint-disable-next-line no-unused-vars
-app.use((erro, req, res, next) => {
-  res.status(500).send({ message: "Erro interno do servidor"});
-});
+//Middlewares para interceptar os erros identificado pela nossa aplicação em toda requisição da nossa API
+app.use(manipulador404);
+
+app.use(manipuladorDeErros);
+
 
 export default app;
